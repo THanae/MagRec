d3a1ac9de6febaf5c5a5a173b712aca3597fa6bd
BIG-BROTHER\tilquin <hanae188@outlook.com>
2018-08-30 16:24:36 +0100

    reformatted function

--- magnetic_reconnection_dir/mag_rec_finder.py
+++ magnetic_reconnection_dir/mag_rec_finder.py
@@ -1,62 +1,70 @@
+from typing import Union
+import os
+
 from magnetic_reconnection_dir.csv_utils import send_dates_to_csv
 from magnetic_reconnection_dir.finder.tests.finder_test import get_possible_reconnection_events
 from magnetic_reconnection_dir.lmn_coordinates import test_reconnection_lmn
 
-import os
 
+def df_magnetic_reconnection_events(probe: Union[int, str], parameters: dict, min_walen: float, max_walen: float,
+                                    start_date: str, end_date: str, radius_to_consider: float,
+                                    noise_when_part1_done: bool, noise_when_part2_done: bool):
+    """
+    Stands for detect and find magnetic reconnection events
+    Sends all possible events for a given probe between given times to a csv file
+    All the detection code can be run from this file. The correlation part is the longest (can last overnight), but the
+    LMN part is relatively short (a few minutes for the Helios probes)
+    :param probe: 1, 2 or ulysses for now
+    :param parameters: parameters to be used for the correlation tests
+    :param min_walen: minimum fraction of the Alfven speed that the event must have at the exhaust
+    :param max_walen: maximum fraction of the Alfven speed that the event must have at the exhaust
+    :param start_date: start date of the analysis, must be a string
+    :param end_date: end date of the analysis, must be a string
+    :param radius_to_consider: maximum radius from the Sun of the events to consider
+    :param noise_when_part1_done: if True, will warn the user when the first part of the program is finished
+    :param noise_when_part2_done: if True, warns the user when the program has finished running
+    :return:
+    """
 
-"""
-All the detection code can be run from this file. The correlation part is the longest (can last overnight), but the 
-LMN part is relatively short (a few minutes for the Helios probes
-"""
+    # During the part 1, changes in correlation are detected
+    possible_reconnection_events = get_possible_reconnection_events(probe=probe, parameters=parameters,
+                                                                    start_time=start_date, end_time=end_date,
+                                                                    radius=radius_to_consider, data_split='yearly')
+    possible_reconnection_dates = [possible_reconnection[0] for possible_reconnection in possible_reconnection_events]
 
-"""
-The following parameters have to be filled in by the parameters.
-Probe: 1, 2 or ulysses for now
-Parameters: parameters to be used for the correlation tests
-Min_walen: minimum fraction of the Alfven speed that the event must have at the exhaust
-Max_walen: maximum fraction of the Alfven speed that the event must have at the exhaust
-Start_date: start date of the analysis, must be a string
-End_date: end date of the analysis, must be a string
-Radius_to_consider: maximum radius from the Sun of the events to consider
-Noise_when_part1_done: if True, will warn the user when the first part of the program is finished
-"""
-probe = 1
-parameters = {'sigma_sum': 2.7, 'sigma_diff': 1.9, 'minutes_b': 5}
-min_walen = 0.9
-max_walen = 1.1
-start_date = '13/12/1974'
-end_date = '15/08/1984'
-radius_to_consider = 1
-noise_when_part1_done = True
+    # The user is warned when part 1 is done
+    if noise_when_part1_done:
+        beep()
 
-"""
-During the first part of the program, changes in correlation are detected by the program
-"""
-possible_reconnection_events = get_possible_reconnection_events(probe=probe, parameters=parameters, start_time=start_date,
-                                                                end_time=end_date, radius=radius_to_consider,
-                                                                data_split='yearly')
+    # The events are then run though a series of tests in LMN coordinates
+    lmn_events = test_reconnection_lmn(event_dates=possible_reconnection_dates, probe=probe, minimum_fraction=min_walen,
+                                       maximum_fraction=max_walen)
+    print(lmn_events)
 
-possible_reconnection_dates = [possible_reconnection[0] for possible_reconnection in possible_reconnection_events]
+    # the possible dates are sent to a csv file
+    file_name = 'probe' + str(probe) + '_reconnection_events' + '.csv'
+    send_dates_to_csv(filename=file_name, events_list=lmn_events, probe=probe, add_radius=True)
+
+    if noise_when_part2_done:
+        for loop in range(5):
+            beep()
 
 
 def beep():
     return os.system("echo '\a'")
 
 
-"""
-If the user wants to be warned when the first part is finished, the program beeps at that point
-"""
-if noise_when_part1_done:
-    beep()
-
-"""
-The events are then run though a series of tests in LMN coordinates
-"""
-lmn_events = test_reconnection_lmn(event_dates=possible_reconnection_dates, probe=probe, minimum_fraction=min_walen,
-                                   maximum_fraction=max_walen)
-
-print(lmn_events)
-file_name = 'probe'+str(probe) + '_reconnection_events' + '.csv'
-send_dates_to_csv(filename=file_name, events_list=lmn_events, probe=probe, add_radius=True)
-
+if __name__ == '__main__':
+    space_probe = 1
+    probe_parameters = {'sigma_sum': 2.7, 'sigma_diff': 1.9, 'minutes_b': 5, 'minutes': 3}
+    probe_min_walen = 0.9
+    probe_max_walen = 1.1
+    probe_start_date = '13/12/1974'
+    probe_end_date = '15/08/1984'
+    probe_radius_to_consider = 1
+    noise1 = True
+    noise2 = True
+    df_magnetic_reconnection_events(probe=space_probe, parameters=probe_parameters, min_walen=probe_min_walen,
+                                    max_walen=probe_max_walen, start_date=probe_start_date, end_date=probe_end_date,
+                                    radius_to_consider=probe_radius_to_consider, noise_when_part1_done=noise1,
+                                    noise_when_part2_done=noise2)
