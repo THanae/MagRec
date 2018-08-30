a1f326758758fcd8686d566292898bd09a83eec5
BIG-BROTHER\tilquin <hanae188@outlook.com>
2018-08-30 14:05:51 +0100

    made the data import compatible with Imp

--- data_handler/data_importer/data_import.py
+++ data_handler/data_importer/data_import.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from data_handler.data_importer.helios_data import HeliosData
+from data_handler.data_importer.imp_data import ImpData
 from data_handler.data_importer.imported_data import ImportedData
 from data_handler.data_importer.ulysses_data import UlyssesData
 
@@ -10,6 +11,8 @@ def get_probe_data(probe: Union[int, str], start_date: str, start_hour: int = 0,
         imported_data = HeliosData(probe=probe, start_date=start_date, start_hour=start_hour, duration=duration)
     elif probe == 'ulysses':
         imported_data = UlyssesData(start_date=start_date, start_hour=start_hour, duration=duration)
+    elif probe == 'imp_8':
+        imported_data = ImpData(start_date=start_date, duration=duration, start_hour=start_hour, probe=probe)
     else:
         raise NotImplementedError('This function has only been implemented for Helios 1, Helios 2 and Ulysses so far')
     return imported_data
