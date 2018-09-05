91d2618ec11dffd3704d30df7f24bcf12189a22c
BIG-BROTHER\tilquin <hanae188@outlook.com>
2018-09-05 16:50:56 +0100

    added AceData

---
+++ data_handler/data_importer/ace_data.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from datetime import timedelta
+from heliopy.data import ace
+
+from data_handler.data_importer.imported_data import ImportedData
+
+
+class AceData(ImportedData):
+    def __init__(self, start_date: str = '01/01/2001', duration: int = 15, start_hour: int = 0, probe: str = 'ace'):
+        """
+        :param start_date: string of 'DD/MM/YYYY'
+        :param duration: int in hours
+        :param start_hour: int from 0 to 23 indicating starting hour of given start_date
+        :param probe: 'ace'
+        """
+        super().__init__(start_date, duration, start_hour, probe)
+        self.data = self.get_imported_data()
+
+        if len(self.data) == 0:
+            raise RuntimeWarning('Created AceData object has retrieved no data: {}'.format(self))
+
+    def __repr__(self):
+        return '{}: at {:%H:%M %d/%m/%Y} by probe {}. Data has {} entries.'.format(self.__class__.__name__,
+                                                                                   self.start_datetime,
+                                                                                   self.probe,
+                                                                                   len(self.data))
+
+    def get_imported_data(self):
+        # only works with imp_8 so far
+        data_b = ace.mfi_h0(self.start_datetime, self.end_datetime)
+        data_b = data_b.data  # data_b was previously a time series
+        data_v = ace.swe_h0(self.start_datetime, self.end_datetime)
+        data_v = data_v.data  # data_b was previously a time series
+        indices = [pd.Timestamp(index).to_pydatetime() for index in data_v.index.values]
+        combined_data = pd.DataFrame(index=indices)
+        iteration = 0
+        for index in indices:
+            interval = 2
+            if iteration != 0 and iteration != len(indices) - 1:
+                interval = (indices[iteration + 1] - indices[iteration - 1]).total_seconds() / 60
+            combined_data.loc[index, 'vp_x'] = data_v.loc[index, 'V_GSE_0']
+            combined_data.loc[index, 'vp_y'] = data_v.loc[index, 'V_GSE_1']
+            combined_data.loc[index, 'vp_z'] = data_v.loc[index, 'V_GSE_2']
+            combined_data.loc[index, 'n_p'] = data_v.loc[index, 'Np']
+            # for now both temperatures are equal to keep it similar to other classes as no separate data was found
+            combined_data.loc[index, 'Tp_par'] = data_v.loc[index, 'Tpr']
+            combined_data.loc[index, 'Tp_perp'] = data_v.loc[index, 'Tpr']
+            combined_data.loc[index, 'r_sun'] = 1 - np.sqrt(
+                data_v.loc[index, 'SC_pos_GSE_0'] ** 2 + data_v.loc[index, 'SC_pos_GSE_1'] ** 2 + data_v.loc[
+                    index, 'SC_pos_GSE_2'] ** 2) * 6.68459e-9  # km to au, 1- because distance initially from earth
+            combined_data.loc[index, 'Bx'] = np.mean(
+                data_b.loc[index - timedelta(minutes=interval):index + timedelta(minutes=interval), 'BGSEc_0'])
+            combined_data.loc[index, 'By'] = np.mean(
+                data_b.loc[index - timedelta(minutes=interval):index + timedelta(minutes=interval), 'BGSEc_1'])
+            combined_data.loc[index, 'Bz'] = np.mean(
+                data_b.loc[index - timedelta(minutes=interval):index + timedelta(minutes=interval), 'BGSEc_2'])
+
+            iteration += 1
+
+        return combined_data
+
+
+if __name__ == '__main__':
+    x = AceData()
+    print(x.data)
--- data_handler/data_importer/data_import.py
+++ data_handler/data_importer/data_import.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from data_handler.data_importer.ace_data import AceData
 from data_handler.data_importer.helios_data import HeliosData
 from data_handler.data_importer.imp_data import ImpData
 from data_handler.data_importer.imported_data import ImportedData
@@ -13,6 +14,8 @@ def get_probe_data(probe: Union[int, str], start_date: str, start_hour: int = 0,
         imported_data = UlyssesData(start_date=start_date, start_hour=start_hour, duration=duration)
     elif probe == 'imp_8':
         imported_data = ImpData(start_date=start_date, duration=duration, start_hour=start_hour, probe=probe)
+    elif probe == 'ace':
+        imported_data = AceData(start_date=start_date, duration=duration, start_hour=start_hour)
     else:
         raise NotImplementedError('This function has only been implemented for Helios 1, Helios 2 and Ulysses so far')
     return imported_data
