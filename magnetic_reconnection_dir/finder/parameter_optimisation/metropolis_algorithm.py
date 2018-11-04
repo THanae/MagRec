0f403658a971736e09efdaa385cbad5d9b781888
THanae <hanae188@outlook.com>
2018-11-04 21:57:58 +0000

    started implementing metropolis algorithm

---
+++ magnetic_reconnection_dir/finder/parameter_optimisation/metropolis_algorithm.py
@@ -0,0 +1,109 @@
+from typing import List, Union
+import numpy as np
+
+from magnetic_reconnection_dir.finder.correlation_finder import CorrelationFinder
+from magnetic_reconnection_dir.finder.parameter_optimisation.mcc_calculations import mcc_from_parameters
+
+
+def fitness_function(parameters: dict) -> List[Union[float, dict]]:
+    """
+    Calculates the fitness function for a given set of parameters
+    :param parameters: parameters to be tested
+    :return: mcc associated with parameters
+    """
+    mcc = mcc_from_parameters(parameters, CorrelationFinder())
+    if np.isnan(mcc[0]):
+        mcc[0] = 0
+    return mcc
+
+
+def metropolis_algorithm(maximum_iterations: int = 100) -> List[Union[float, dict]]:
+    """
+    Finds the best possible parameters by randomly generating parameters
+    Moves through the parameter hyperspace with the metropolis algorithm
+    :param maximum_iterations: maximum number of iterations before the code stops running, should take into account
+    burn-in period
+    :return: best mcc obtained, with associated parameters
+    """
+    initial_parameters = {'sigma_sum': np.random.uniform(1.9, 3.1), 'sigma_diff': np.random.uniform(1.9, 3.1),
+                          'minutes_b': np.random.uniform(1, 10), 'minutes': np.random.uniform(1, 10),
+                          'minimum walen': np.random.uniform(0.6, 1), 'maximum walen': np.random.uniform(1, 1.4)}
+    current_parameters = initial_parameters.copy()
+    current_iteration = 0
+    mcc, max_mcc = [0, {}], [0, {}]
+    mcc_calculated = False
+    while current_iteration < maximum_iterations and mcc[0] < 0.9:
+        if mcc_calculated:
+            mcc = max_mcc
+        else:
+            mcc = fitness_function(current_parameters)
+            max_mcc = mcc.copy()
+        mcc_calculated = False
+        previous_parameters = current_parameters.copy()
+        current_parameters = parameters_from_hypersphere(current_parameters)
+        challenging_mcc = fitness_function(current_parameters)
+        if challenging_mcc[0] > max_mcc[0]:
+            max_mcc = challenging_mcc
+            current_iteration += 1
+        else:
+            current_parameters = metropolis(previous_parameters, current_parameters, max_mcc[0], challenging_mcc[0])
+            if current_parameters == previous_parameters:
+                mcc_calculated = True
+        print(f'Iteration {current_iteration}: MCC {max_mcc[0]} with parameters {current_parameters}')
+    return max_mcc
+
+
+def metropolis(best_parameters: dict, worse_parameters: dict, best_mcc: float, worse_mcc: float):
+    """
+    Determines whether the center of the hypersphere moves or not following the probability worse_mcc/best_mcc
+    :param best_parameters: parameters with best mcc
+    :param worse_parameters: parameters with worse mcc
+    :param best_mcc: best mcc
+    :param worse_mcc: worse mcc
+    :return: parameters to be used
+    """
+    try:
+        switch_probability = worse_mcc / best_mcc
+    except ZeroDivisionError:
+        return worse_parameters
+    if np.random.uniform(0, 1) <= switch_probability:
+        parameters = worse_parameters
+    else:
+        parameters = best_parameters
+    return parameters
+
+
+def parameters_from_hypersphere(parameters: dict, max_radius: float = 2) -> dict:
+    """
+    Generates set of parameters within the sphere of radius max_radius, centered at parameters
+    :param parameters: parameters at the center of the sphere
+    :param max_radius: radius of the sphere in the parameters hyperspace
+    :return: new set of parameters within the sphere
+    """
+    radius = max_radius + 1
+    new_parameters = {}
+    while radius > max_radius:
+        new_parameters = {'sigma_sum': np.random.uniform(1.9, 3.1), 'sigma_diff': np.random.uniform(1.9, 3.1),
+                          'minutes_b': np.random.uniform(1, 10), 'minutes': np.random.uniform(1, 10),
+                          'minimum walen': np.random.uniform(0.6, 1), 'maximum walen': np.random.uniform(1, 1.4)}
+        radius = radius_of_parameters(parameters, new_parameters)
+    return new_parameters
+
+
+def radius_of_parameters(parameters1: dict, parameters2: dict) -> float:
+    """
+    Finds the distance between two sets of parameters in the parameter hyperspace
+    :param parameters1: center set of parameters
+    :param parameters2: outer set of parameters
+    :return: distance between the two sets of parameters
+    """
+    radius = 0
+    for key in parameters1.keys():
+        radius += (parameters1[key] - parameters2[key]) ** 2
+    radius = np.sqrt(radius)
+    return radius
+
+
+if __name__ == '__main__':
+    mcc1 = metropolis_algorithm(maximum_iterations=100)
+    # TODO plot progression
